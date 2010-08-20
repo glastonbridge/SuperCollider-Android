ef171efeefce576e3c069f1eb94024e7d2ffcd5c
Dan Stowell <danstowell@users.sourceforge.net>
2010-08-20 10:45:02 +0100

    Add AY_UGen to what getsbuilt

--- Application.mk
+++ Application.mk
@@ -1,5 +1,6 @@
 APP_PROJECT_PATH := $(call my-dir)
 APP_MODULES      := scsynth PanUGens FilterUGens BinaryOpUGens IOUGens NoiseUGens LFUGens OscUGens MulAddUGens UnaryOpUGens TriggerUGens DelayUGens GendynUGens ReverbUGens \
 	MCLDBufferUGens MCLDTreeUGens MCLDTriggeredStatsUgens \
+	AY_UGen \
         libsndfile 
 # MCLDFFTUGens not yet, pending fft lib availability (not for own use but for fft lib header inclusion)
