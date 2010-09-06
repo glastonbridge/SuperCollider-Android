28ea1fbc64780b1b683eebbb678cf77d5a9eba85
Dan Stowell <danstowell@users.sourceforge.net>
2010-09-06 20:48:52 +0100

    correct a post

--- jni/Source/server/SC_Android.cpp
+++ jni/Source/server/SC_Android.cpp
@@ -351,7 +351,7 @@ extern "C" void scsynth_android_doOsc(JNIEnv* env, jobject classobj, jobjectArra
         makePacket(env,oscMessage,packet);
         World_SendPacket(world,((packet.size()+4)>>2)*4,  (char*)packet.buf, androidReplyFunc);
     }else{
-    	scprintf("scsynth_android_makeSynth: not running!\n");
+    	scprintf("scsynth_android_doOsc: not running!\n");
     }
 }
 
